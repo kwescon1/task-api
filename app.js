a93d046da34d48d6d99b3b4d8a26bcc966833d2d
Odame <kwesi.danquah@daaseng.com>
2024-02-20 00:51:51 +0000

    Configure express app

---
+++ app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import { responseMacro } from "./middlewares/responseMacro.js";
+
+// import statements
+import router from "./router/index.js";
+
+// application initialization
+const app = express();
+
+// Middleware
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(responseMacro);
+
+// Use router
+app.use("/api/v1", router);
+
+// router.get("/try", (req, res) => {
+//   res.send("Tasks route is working");
+// });
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});

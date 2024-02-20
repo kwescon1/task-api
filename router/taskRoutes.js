2f20b5fe92f34fbb2942efc2a1947e514d90beb7
Odame <kwesi.danquah@daaseng.com>
2024-02-20 00:50:32 +0000

    Define task-related routes and link to controller actions

---
+++ router/taskRoutes.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { taskController } from "../controllers/taskController.js";
+import { validateTask } from "../requests/TaskRequest.js";
+
+const taskRoutes = express.Router();
+
+// Apply the validateTake request to the controller
+taskRoutes.post("/", validateTask, taskController.store);
+
+taskRoutes.get("/", taskController.index);
+taskRoutes.put("/:id", taskController.update);
+taskRoutes.get("/:id", taskController.show);
+taskRoutes.delete("/:id", taskController.destroy);
+
+export default taskRoutes;

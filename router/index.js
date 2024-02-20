0c19807e9bca53f19a6a7928682684b9a4590b3f
Odame <kwesi.danquah@daaseng.com>
2024-02-20 00:50:16 +0000

    Configure base router to include task routes

--- router/index.js
+++ router/index.js
@@ -1,6 +1,10 @@
 import express from "express";
+import taskRoutes from "./taskRoutes.js";
 
 // Create router instance
 const router = express.Router();
 
+router.use("/tasks", taskRoutes);
+
+// export configured routes
 export default router;

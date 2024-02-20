2fcdbb478a349a1a7f71335f1b4a306e8d2c5770
Odame <kwesi.danquah@daaseng.com>
2024-02-20 00:51:26 +0000

    Add TaskRequest validation for task creation and updates

---
+++ requests/TaskRequest.js
@@ -0,0 +1,23 @@
+import { body, validationResult } from "express-validator";
+import { StatusCodes } from "http-status-codes";
+
+export const validateTask = [
+  // Title is required and must be a string
+  body("title")
+    .trim()
+    .isLength({ min: 3 })
+    .withMessage("Title is required.")
+    .isString()
+    .withMessage("Title must be a string."),
+
+  // Custom middleware to check the validation result
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ errors: errors.array() });
+    }
+    next();
+  },
+];

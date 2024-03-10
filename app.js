a4ed60917b2da69a074988aaef1a938824d748bf
Odame <kwesi.danquah@daaseng.com>
2024-03-10 13:59:54 +0000

    Code refactoring. implemented kernel in application start up

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ dotenv.config();
 
 import express from "express";
 import DatabaseConnection from "./services/db/DatabaseService.js";
-import { responseMacro } from "./middlewares/responseMacro.js";
+import Kernel from "./app/Http/Kernel.js"; // Import the Kernel class
 import router from "./router/index.js";
+// import { errorHandler } from "./exceptions/Handler.js";
 
 // application initialization
 const app = express();
@@ -18,14 +19,13 @@ const dbConnection = new DatabaseConnection(mongoUri);
 
 dbConnection.connect();
 
-// Middleware
-app.use(express.json()); // for parsing application/json
-app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-// app.use(bodyParser({ extended: false }));
-app.use(responseMacro);
+const kernel = new Kernel(app); // Instantiate Kernel with the app
+kernel.applyMiddleware(); // Apply all configured middleware
 
 // Use router
 app.use("/api/v1", router);
+
+// app.use(errorHandler);
 
 const port = process.env.PORT;
 app.listen(port, () => {

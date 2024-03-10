df80a3612d67add6cfa061d63056f3dd39276e57
Odame <kwesi.danquah@daaseng.com>
2024-03-10 23:46:32 +0000

    Refactored  TaskController to Class-Based Structure: Transitioned TaskController from function-based to class-based structure to align with DI implementation and enhance code organization

--- app/Http/Controllers/TaskController.js
+++ app/Http/Controllers/TaskController.js
@@ -1,52 +1,100 @@
-// controllers/taskController.js
-import * as taskService from "../../Modules/Task/Service/TaskService.js";
+import logger from "../../../config/logging.js";
+/**
+ * Class representing the controller for task operations.
+ * This controller handles all task-related HTTP requests.
+ */
+class TaskController {
+  /**
+   * TaskController constructor.
+   * Injects TaskService for handling the business logic.
+   * @param {TaskService} taskService - The service for task operations.
+   */
+  constructor({ taskService }) {
+    this.taskService = taskService;
+  }
 
-export const taskController = {
-  index: async (req, res) => {
+  /**
+   * Handles the request to list all tasks.
+   * @param {Object} req - The Express request object.
+   * @param {Object} res - The Express response object.
+   */
+  async index(req, res) {
     try {
-      const tasks = await taskService.getAllTasks();
+      const tasks = await this.taskService.getAllTasks();
       res.success(tasks, "Tasks retrieved successfully");
     } catch (error) {
-      res.error("Failed to retrieve tasks");
+      res.error("Failed to retrieve tasks", error);
     }
-  },
+  }
 
-  store: async (req, res) => {
+  /**
+   * Handles the request to create a new task.
+   * @param {Object} req - The Express request object.
+   * @param {Object} res - The Express response object.
+   */
+  async store(req, res) {
     try {
-      const newTask = await taskService.addTask(req.body);
+      const newTask = await this.taskService.addTask(req.body);
       res.created(newTask, "Task created successfully");
     } catch (error) {
-      res.error("Failed to create task");
+      res.error("Failed to create task", error);
     }
-  },
+  }
 
-  update: async (req, res) => {
+  /**
+   * Handles the request to update an existing task.
+   * @param {Object} req - The Express request object.
+   * @param {Object} res - The Express response object.
+   */
+  async update(req, res) {
     try {
-      const updatedTask = await taskService.updateTask(req.params.id, req.body);
+      const updatedTask = await this.taskService.updateTask(
+        req.params.id,
+        req.body
+      );
       if (!updatedTask) {
         return res.notFound("Task not found");
       }
       res.success(updatedTask, "Task updated successfully");
     } catch (error) {
-      res.error("Failed to update task");
+      res.error("Failed to update task", error);
     }
-  },
+  }
 
-  show: async (req, res) => {
-    const task = await taskService.getTask(req.params.id);
-
-    res.success(task);
-  },
-
-  destroy: async (req, res) => {
+  /**
+   * Handles the request to retrieve a single task by its ID.
+   * @param {Object} req - The Express request object.
+   * @param {Object} res - The Express response object.
+   */
+  async show(req, res) {
     try {
-      const success = await taskService.deleteTask(req.params.id);
+      const task = await this.taskService.getTask(req.params.id);
+      if (!task) {
+        return res.notFound("Task not found");
+      }
+      res.success(task);
+    } catch (error) {
+      logger.error(error);
+      res.error("Failed to retrieve task", error);
+    }
+  }
+
+  /**
+   * Handles the request to delete a task by its ID.
+   * @param {Object} req - The Express request object.
+   * @param {Object} res - The Express response object.
+   */
+  async destroy(req, res) {
+    try {
+      const success = await this.taskService.deleteTask(req.params.id);
       if (!success) {
         return res.notFound("Task not found");
       }
       res.success(null, "Task deleted successfully");
     } catch (error) {
-      res.error("Failed to delete task");
+      res.error("Failed to delete task", error);
     }
-  },
-};
+  }
+}
+
+export default TaskController;

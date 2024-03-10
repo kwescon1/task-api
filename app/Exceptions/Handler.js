cb652443111c76b051defa77020e7aa491f232ad
Odame <kwesi.danquah@daaseng.com>
2024-03-10 16:54:56 +0000

    General error handler

---
+++ app/Exceptions/Handler.js
@@ -0,0 +1,46 @@
+// Import the logger
+import { StatusCodes } from "http-status-codes";
+import logger from "../../config/logging.js";
+
+/**
+ * Error handling middleware class to log errors and send appropriate responses.
+ */
+class ErrorHandler {
+  /**
+   * Handles errors by logging and responding with the appropriate error message and status code.
+   * @param {Object} err - The error object.
+   * @param {Object} req - Express request object.
+   * @param {Object} res - Express response object.
+   * @param {Function} next - Callback to pass control to the next middleware function.
+   */
+  handle(err, req, res, next) {
+    // Extract useful error information
+    const errorInfo = {
+      method: req.method,
+      url: req.originalUrl,
+      ip: req.ip,
+      error: err.message,
+      stack: err.stack.split("\n")[1].trim(), // Getting the first line of the stack trace
+    };
+
+    // Log the error details
+    logger.error(JSON.stringify(errorInfo));
+
+    // Switch case on the error type
+    switch (err.constructor.name) {
+      case "NotFoundException":
+        res.notFound(err.message);
+        break;
+      // Add more cases as needed for other custom errors
+      default:
+        // Handle unexpected errors
+        res.error(
+          err.message || "Internal Server Error",
+          err.status || StatusCodes.INTERNAL_SERVER_ERROR
+        );
+    }
+  }
+}
+
+// Export the ErrorHandler class
+export default new ErrorHandler();
